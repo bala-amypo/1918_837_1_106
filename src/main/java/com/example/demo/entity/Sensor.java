54d3b4717f07bf227f113ab9c6e8035712ff09a1
bala-amypo <bala@amypo.in>
2025-12-18 17:01:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,11 +1,16 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class Sensor{
     @Id
     private Long id;
-    @
+    @column(unique=true)
+    private String sensorCode;
+    private String sensorType;
+    private String location;
+    
     
 }
