e0e77bf0f7bc16f53035b8b33bc64a643e80d042
bala-amypo <bala@amypo.in>
2025-12-25 18:50:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,25 +1,9 @@
-package com.example.demo;
+package com.example.demo.repository;
 
-import com.example.demo.service.impl.UserServiceImpl;
-import com.example.demo.repository.UserRepository;
-import org.mockito.Mockito;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public class FullProjectTest {
-
-    private UserRepository userRepository;
-    private UserServiceImpl userService;
-
-    @BeforeMethod
-    public void setup() {
-        userRepository = Mockito.mock(UserRepository.class);
-        userService = new UserServiceImpl(userRepository);
-    }
-
-    @Test
-    public void sampleTest() {
-        Assert.assertTrue(true);
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
