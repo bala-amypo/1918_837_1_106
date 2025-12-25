a5acf0fafff8fa94e71674f4b749f51158e45964
bala-amypo <bala@amypo.in>
2025-12-25 16:53:35 +0000

    User commit

--- FullProjectTest.java
+++ FullProjectTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class FullProjectTest {
 
     @Test
-    public void contextLoads() {
+    public void smokeTest() {
         Assert.assertTrue(true);
     }
 }
