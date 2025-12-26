64a0c4478ec1e3a1665d20d436ca712a730aa7cb
bala-amypo <bala@amypo.in>
2025-12-26 06:35:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SensorReadingServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SensorReadingServiceImpl.java
@@ -1,11 +1,34 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.SensorReading;
+import com.example.demo.repository.SensorReadingRepository;
+import com.example.demo.service.SensorReadingService;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface SensorReadingService {
-    SensorReading save(SensorReading reading);
-    List<SensorReading> findAll();
-    Optional<SensorReading> findById(Long id);
+@Service
+public class SensorReadingServiceImpl implements SensorReadingService {
+
+    private final SensorReadingRepository sensorReadingRepository;
+
+    public SensorReadingServiceImpl(SensorReadingRepository sensorReadingRepository) {
+        this.sensorReadingRepository = sensorReadingRepository;
+    }
+
+    @Override
+    public SensorReading save(SensorReading reading) {
+        return sensorReadingRepository.save(reading);
+    }
+
+    @Override
+    public List<SensorReading> findAll() {
+        return sensorReadingRepository.findAll();
+    }
+
+    @Override
+    public Optional<SensorReading> findById(Long id) {
+        return sensorReadingRepository.findById(id);
+    }
 }
