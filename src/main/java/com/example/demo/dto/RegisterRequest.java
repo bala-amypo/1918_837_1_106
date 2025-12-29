6d3bd96776ffb85d8dfe49ea8fc996df210318ac
bala-amypo <bala@amypo.in>
2025-12-29 04:57:27 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,47 +1,14 @@
 package com.example.demo.dto;
 
-public class ApiResponse {
-
-    private boolean success;
-    private String message;
-    private Object data;
-
-    public ApiResponse() {
-    }
-
-    public ApiResponse(boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
-
-    public ApiResponse(boolean success, String message, Object data) {
-        this.success = success;
-        this.message = message;
-        this.data = data;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
+public class RegisterRequest {
+    private String email;
+    private String password;
+    
+    public RegisterRequest() {}
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
-
